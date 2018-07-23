21f0f12dc96ca6e1f839b159d39e764dd99f306c
tetsuobe <tetsuobe@gmail.com>
2018-07-23 09:53:23 +0200

    Scrape Adong menu

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const request = require('request');
+const cheerio = require('cheerio');
 
-request('http://example.org', function (error, response, html) {
+const url = 'http://www.adong.gdansk.pl/menu/gdansk';
+
+request(url, function (error, response, html) {
   if (error) {
     throw Error(error);
   }
 
-  console.log(html);
+  const $ = cheerio.load(html);
+  let menu = [];
+
+  $('.dish').each(function () {
+    let data = $(this);
+    let price = data.find('.dishPT').text().replace(/^\s*x\s/g, '').split(',');
+    let dish = {
+      position: parseInt(data.find('.dishNumT').text()),
+      name: data.find('.dishName').text(),
+      price: {
+        zl: parseInt(price[0]),
+        gr: parseInt(price[1]),
+      }
+    }
+    menu.push(dish);
+  })
+
+  console.log(menu);
 });
