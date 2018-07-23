2d63348da47f28e23fad48b503617fea89e368a6
tetsuobe <tetsuobe@gmail.com>
2018-07-23 15:38:31 +0200

    SurfBurger fies added

--- scrapers/surfburger.js
+++ scrapers/surfburger.js
@@ -1,12 +1,13 @@
 module.exports = function ({ cheerio, html }) {
   const $ = cheerio.load(html)
   let menu = []
+  let index = 0
 
-  $('#menu div:not(:last-child) .wpb_column.vc_column_container.vc_col-sm-3:nth-last-child(n+2)').each(function (index) {
+  $('#menu div:not(:last-child) .wpb_column.vc_column_container.vc_col-sm-3:nth-last-child(n+1)').each(function () {
     let data = $(this)
     let name = data.find('h5').text().split(/\n/)
     let dish = {
-      position: index + 1,
+      position: ++index,
       name: name[0].trim(),
       description: data.find('p:last-child').text().trim(),
       price: {
@@ -16,6 +17,27 @@ module.exports = function ({ cheerio, html }) {
     }
     menu.push(dish)
   })
+
+  const fries = $('#menu .vc_row.wpb_row.vc_inner.vc_row-fluid:last-child .wpb_column.vc_column_container.vc_col-sm-3:nth-child(4)')
+  const fryPrefix = fries.find('h3').text()
+  const fryDescription = fries.find('p:nth-child(3)').text()
+  const fryNames = fries.find('h5').text().split(/[;,:]-/)
+  fryNames.forEach(function (name) {
+    let data = name.trim()
+    if (data == null || data === '') {
+      return
+    }
+    let dish = {
+      position: ++index,
+      name: `${fryPrefix} - ${data.substr(0, data.length - 2)}`,
+      description: fryDescription,
+      price: {
+        zl: parseInt(data.substr(-1, 1)),
+        gr: 0
+      }
+    }
+    menu.push(dish)
+  }, {fryPrefix, fryDescription})
 
   return menu
 }
